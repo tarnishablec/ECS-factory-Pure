c1d33a6d4a9ba7ea3c12e3b36661125fd07926c7
tarnishablec <tarnishablec@outlook.com>
2019-04-01 01:24:35 +0800

    json.net

---
+++ ecs-factory-display/Assets/Scripts/Bootstraps/RobotEntityBootstrap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class RobotEntityBootstrap : Bootstrap
+{
+    RobotEntityBootstrap instace = GetBootstrap<RobotEntityBootstrap>();
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    public static void Boot()
+    {
+
+    }
+}
---
+++ ecs-factory-display/Assets/Scripts/Common/RobotPool.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public static class RobotPool
+{
+    public static Dictionary<string,Dictionary<string,string>> robotsData= new Dictionary<string, Dictionary<string, string>>();
+}
