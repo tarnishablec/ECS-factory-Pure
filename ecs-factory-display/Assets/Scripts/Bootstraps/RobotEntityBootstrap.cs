98d0f5748718becfa8eef40e866e454d43e9b7a5
tarnishablec <tarnishablec@outlook.com>
2019-04-01 02:44:47 +0800

    websocket

--- ecs-factory-display/Assets/Scripts/Bootstraps/RobotEntityBootstrap.cs
+++ ecs-factory-display/Assets/Scripts/Bootstraps/RobotEntityBootstrap.cs
@@ -5,7 +5,6 @@ using Unity.Entities;
 
 public class RobotEntityBootstrap : Bootstrap
 {
-    RobotEntityBootstrap instace = GetBootstrap<RobotEntityBootstrap>();
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
     public static void Boot()
---
+++ ecs-factory-display/Assets/Scripts/Bootstraps/RobotsDataWebSocketBootstrap.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotsDataWebSocketBootstrap : MonoBehaviour
+{
+    public string uri;
+
+    private WebSocketClient socketClient;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        socketClient = new WebSocketClient(uri);
+        socketClient.Connect();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
