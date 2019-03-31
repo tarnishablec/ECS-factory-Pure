25e51551ad68381ef78794158e984ab7281970d4
tarnishablec <tarnishablec@outlook.com>
2019-04-01 07:46:51 +0800

    update

--- ecs-factory-display/Assets/Scripts/Systems/UpdateTargetSystem.cs
+++ ecs-factory-display/Assets/Scripts/Systems/UpdateTargetSystem.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Entities;
+using Unity.Mathematics;
 
 public class UpdateTargetSystem : ComponentSystem
 {
+    struct Group
+    {
+        public readonly int Length;
+        public ComponentDataArray<RobotNameComponent> robotNames;
+        public ComponentDataArray<TargetPositionComponent> targetPositions;
+    }
 
+    [Inject]
+    Group group;
 
     protected override void OnUpdate()
     {
+        var robotsInfo = RobotPool.robotsData;
+        for (int i = 0; i < group.Length; i++)
+        {
+        }
     }
 }
